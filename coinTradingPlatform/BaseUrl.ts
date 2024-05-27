6de465df0748e4fb412ab92b5b5de2530e470aa0
nileshpanhale1601 <nileshpanhale1601@gmail.com>
2024-05-27 13:45:08 +0000

    backend server url link updated

--- coinTradingPlatform/BaseUrl.ts
+++ coinTradingPlatform/BaseUrl.ts
@@ -1,6 +1,6 @@
 // const baseUrl = "https://server.orogram.io/v1"
-// const baseUrl = "http://34.130.60.37:3009/v1"
+const baseUrl = "http://34.123.109.10:3000/v1"
 // const baseUrl = "http://127.0.0.1:3009/v1"
-const baseUrl = "http://localhost:3009/v1"
+// const baseUrl = "http://localhost:3009/v1"
 
 export default baseUrl;
